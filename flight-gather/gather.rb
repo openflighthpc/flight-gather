f1fbe1f184101317c51fcabe3f472587ae3c7c01
Matt Ruddy <matt@calcwise.com>
2022-10-04 18:02:48 +0100

    Initial version
    Missing some functionality

---
+++ flight-gather/gather.rb
@@ -0,0 +1,115 @@
+#!/usr/bin/ruby -w
+
+require "yaml"
+require "optparse"
+require "socket"
+require "active_support"
+require "active_support/core_ext/hash"
+require "json"
+require "pp"
+
+# Set command line options
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "A tool to gather system information for a node.\nUsage: gather.rb [options]"
+  opts.on("-p", "--primary PRIMARYGROUPS", "Primary group for the node") { |o| options[:pri] = o }
+  opts.on("-g", "--groups GROUP1,GROUP2,GROUP3", Array, "Comma-separated list of secondary groups for the node") { |o| options[:sec] = o }
+  opts.on("-n", "--name FILENAME", "Name of exported YAML file") { |o| options[:name] = o }
+end.parse!
+
+data = { system: { model: "NULL",
+                   bios: "NULL",
+                   serial: "NULL",
+                   cpus: {},
+                   gpus: {},
+                   ram: "NULL",
+                   cloud: "NULL",
+                   primaryGroup: options[:pri],
+                   secondaryGroups: options[:sec]
+                 },
+                   
+         network: {},
+         disks: {}
+        }
+
+# Get system info
+data[:system][:model] = `dmidecode -s system-product-name`.delete("\n")
+data[:system][:bios] = `dmidecode -s bios-version`.delete("\n")
+data[:system][:serial] = `dmidecode -s system-serial-number`.delete("\n")
+data[:system][:ram] = `grep MemTotal /proc/meminfo | awk '{print $2}'`.delete("\n") # RAM measured in kB
+
+# Get processor info
+procIds = `dmidecode -q -t processor | grep "ID:"`.delete("\t").split("\n").map{ |x| x[4..] } # collect CPU IDs into array
+procInfo = JSON.parse(`lscpu -J`)["lscpu"]
+procIds.each do |id|
+  data[:system][:cpus][id] = {}
+  procInfo.each do |setting|
+    if setting["field"] == "Model name:"
+      data[:system][:cpus][id][:model] = setting["data"]
+    elsif setting["field"] == "Core(s) per socket:"
+      data[:system][:cpus][id][:cores] = setting["data"].to_i
+    end
+    data[:system][:cpus][id][:slot] = `dmidecode -q -t processor | grep "Socket Designation:"`[21..-2]
+  end
+end
+
+#procInfo = `dmidecode -q -t processor`.split("\n")
+#for procIndex in 0..(procInfo.grep(/ID:/).length-1)
+#  id = procInfo.grep(/ID:/)[procIndex][5..]
+#  data[:system][:cpus][id] = { model: procInfo.grep(/Version:/)[procIndex][10..],
+#                               slot: procInfo.grep(/Socket Designation:/)[procIndex][21..],
+#                               cores: procInfo.grep(/Core Count:/)[procIndex][13..].to_i
+#                             }
+#end
+
+# Get interface info
+ifs = Socket.getifaddrs.select { |x| x.addr.ipv4?}
+ifs.each do |interface|
+  data[:network][interface.name] = { ip: interface.addr.ip_address,
+                                     mac: `nmcli -t device show #{interface.name} | grep HWADDR`[15..31],
+                                     speed: `ethtool #{interface.name} | grep Speed | awk '{print $2}'`.delete("\n")
+                                   }
+end
+
+#ifs = XmlHasher.parse(`lshw -C network -xml`)[:list][:node]
+#ifs = [ifs].flatten(1) # convert to singleton array if not an array already
+#ifs.each do |interface|
+#  data[:network][interface[:logicalname]] = { #mac: interface[:serial],
+#                                              speed: interface[:size]
+#                                            }
+#  interface[:configuration][:setting].each do |setting|
+#    if setting[:id] == "ip"
+#      data[:network][interface[:logicalname]][:ip] = setting[:value]
+#    end
+#  end
+#end
+
+# Get disk size
+diskText = `lsblk -d`.split("\n").drop(1)
+diskText.each do |disk|
+  diskData = disk.split
+  data[:disks][diskData[0]] = {size: diskData[3]}
+end
+
+# Get GPU info
+gpus = Hash.from_xml(`lshw -C display -xml`)["list"]["node"]
+gpus = [gpus].flatten(1) # convert to singleton array if not an array already
+gpus.each_with_index do |gpu, index|
+  data[:system][:gpus]["GPU #{index}"] = { name: gpu["product"],
+                                           slot: gpu["handle"]
+                                         }
+end
+
+# Get cloud platform info
+sysInfo = `dmidecode -t system`.downcase
+if sysInfo.include? "openstack"
+  data[:system][:cloud] = "OpenStack"
+elsif sysInfo.include? "amazon"
+  data[:system][:cloud] = "Amazon"
+elsif sysInfo.include? "azure"
+  data[:system][:cloud] = "Azure"
+else
+  data[:system][:cloud] = "Unrecognised cloud platform"
+end
+
+File.open("./#{options[:name]}.yml", "w") { |file| file.write(data.to_yaml) }
