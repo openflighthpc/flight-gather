debc5318cb6c76b49bf4948fa6664d294fc430da
Matt Ruddy <matt@calcwise.com>
2022-10-05 16:34:19 +0100

    Restructure data hash

--- flight-gather/gather.rb
+++ flight-gather/gather.rb
@@ -22,31 +22,29 @@ OptionParser.new do |opts|
   opts.on("-d", "--directory DIRECTORY", "Directory to save output to, defaults to current directory") { |o| options[:dir] = o }
 end.parse!
 
-data = { system: { model: "NULL",
-                   bios: "NULL",
-                   serial: "NULL",
-                   cpus: {},
-                   gpus: {},
-                   ram: "NULL",
-                   cloud: "NULL",
-                   primaryGroup: options[:pri],
-                   secondaryGroups: options[:sec]
-                 },
-                   
+data = { model: "NULL",
+         bios: "NULL",
+         serial: "NULL",
+         cpus: {},
+         gpus: {},
+         ram: "NULL",
+         cloud: "NULL",
+         primaryGroup: options[:pri],
+         secondaryGroups: options[:sec],
          network: {},
          disks: {}
         }
 
 # Get system info
-data[:system][:model] = `dmidecode -s system-product-name`.delete("\n")
-data[:system][:bios] = `dmidecode -s bios-version`.delete("\n")
-data[:system][:serial] = `dmidecode -s system-serial-number`.delete("\n")
-data[:system][:ram] = `grep MemTotal /proc/meminfo | awk '{print $2}'`.delete("\n") # RAM measured in kB
+data[:model] = `dmidecode -s system-product-name`.delete("\n")
+data[:bios] = `dmidecode -s bios-version`.delete("\n")
+data[:serial] = `dmidecode -s system-serial-number`.delete("\n")
+data[:ram] = `grep MemTotal /proc/meminfo | awk '{print $2}'`.delete("\n") # RAM measured in kB
 
 # Get processor info
 procInfo = `dmidecode -q -t processor`.split("Processor Information")[1..]
 procInfo.each do |proc|
-  data[:system][:cpus][between(proc, "Socket Designation: ", "\n")] = { id: between(proc, "ID: ", "\n",),
+  data[:cpus][between(proc, "Socket Designation: ", "\n")] = { id: between(proc, "ID: ", "\n",),
                                                                       model: between(proc, "Version: ", "\n",),
                                                                       cores: [between(proc, "Core Count: ", "\n").to_i, 1].max
                                                                     }
@@ -72,7 +70,7 @@ end
 gpus = Hash.from_xml(`lshw -C display -xml`)["list"]["node"]
 gpus = [gpus].flatten(1) # convert to singleton array if not an array already
 gpus.each_with_index do |gpu, index|
-  data[:system][:gpus]["GPU #{index}"] = { name: gpu["product"],
+  data[:gpus]["GPU #{index}"] = { name: gpu["product"],
                                            slot: gpu["handle"]
                                          }
 end
@@ -80,13 +78,13 @@ end
 # Get cloud platform info
 sysInfo = `dmidecode -t system`.downcase
 if sysInfo.include? "openstack"
-  data[:system][:cloud] = "OpenStack"
+  data[:cloud] = "OpenStack"
 elsif sysInfo.include? "amazon"
-  data[:system][:cloud] = "AWS"
+  data[:cloud] = "AWS"
 elsif sysInfo.include? "azure"
-  data[:system][:cloud] = "Azure"
+  data[:cloud] = "Azure"
 else
-  data[:system][:cloud] = "Not on a cloud platform"
+  data[:cloud] = "Not on a cloud platform"
 end
 
 File.open(options[:dir]+options[:name]+".yml", "w") { |file| file.write(data.to_yaml) }
