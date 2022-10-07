eddd067a663e99b67e273efff114cd8b0a636fd1
Matt Ruddy <matt@calcwise.com>
2022-10-07 16:56:13 +0100

    Improve combatibility with older Ruby versions

--- flight-gather/gather.rb
+++ flight-gather/gather.rb
@@ -22,7 +22,7 @@ OptionParser.new do |opts|
   opts.banner = "A tool to gather system information for a node.\nUsage: gather.rb [options]"
   opts.on("-p", "--primary PRIMARYGROUPS", "Primary group for the node") { |o| options[:pri] = o }
   opts.on("-g", "--groups x,y,z", Array, "Comma-separated list of secondary groups for the node") { |o| options[:sec] = o }
-  opts.on("-n", "--name FILENAME", "Name of exported YAML file, defaults to data.yml") { |o| if o[-4..] == ".yml" then options[:name] = o else options[:name] = o + ".yml" end }
+  opts.on("-n", "--name FILENAME", "Name of exported YAML file, defaults to data.yml") { |o| if o[-4..-1] == ".yml" then options[:name] = o else options[:name] = o + ".yml" end }
   opts.on("-d", "--directory DIRECTORY", "Directory to save output to, defaults to current directory") { |o| options[:dir] = o }
   opts.on("-t", "--types x,y,z", Array, "Type of check to run (physical or logical), if not provided then both types are collected") { |o| 
                                                                                                                                        options[:physical] = o.include? "physical"
@@ -45,7 +45,7 @@ end
 # Get processor info
 if options[:physical]
   data[:cpus] = {}
-  procInfo = `dmidecode -q -t processor`.split("Processor Information")[1..]
+  procInfo = `dmidecode -q -t processor`.split("Processor Information")[1..-1]
   procInfo.each do |proc|
     data[:cpus][between(proc, "Socket Designation: ", "\n")] = { id: between(proc, "ID: ", "\n",),
                                                                  model: between(proc, "Version: ", "\n",),
