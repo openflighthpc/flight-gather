b116e4da10b7ff957b8a49be03e2827e00f9896e
Jack Millard <millard64@hotmail.co.uk>
2024-06-04 14:42:13 +0100

    Bump version

--- lib/gather/version.rb
+++ lib/gather/version.rb
@@ -27,5 +27,5 @@
 # https://github.com/openflighthpc/flight-gather
 #==============================================================================
 module Gather
-  VERSION = '0.1.1'
+  VERSION = '0.2.0'
 end
